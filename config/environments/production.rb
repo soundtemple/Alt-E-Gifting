abca07a79712dd7d0827abf4d1b2041014d56aa5
Tim Walter <twwalter@gmail.com>
2017-01-14 17:27:18 +1100

    gmail auth details update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
