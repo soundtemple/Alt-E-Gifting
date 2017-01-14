29845416515e668496a4ecac2962527ef737a410
Tim Walter <twwalter@gmail.com>
2017-01-14 18:19:55 +1100

    gmail blocking authentication fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => "alte.gifting",
-   :password             => "@lteGifting",
+   :user_name            => ENV["GMAIL_USERNAME"],
+   :password             => ENV["GMAIL_PASSWORD"],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
