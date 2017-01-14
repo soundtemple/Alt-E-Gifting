abe9ad00641ee3871bbb07b4d1d648e9bbd29555
Tim Walter <twwalter@gmail.com>
2017-01-14 18:27:39 +1100

    gmail tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV["GMAIL_USERNAME"],
-   :password             => ENV["GMAIL_PASSWORD"],
+   :user_name            => "alte.gifting",
+   :password             => "@lteGifting",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
